8a012a8453952be664ce7a1528291ff1d6a6a141
Michelle <michmac202@gmail.com>
2020-07-29 17:59:09 -0500

    3d graph

--- essay/src/App.js
+++ essay/src/App.js
@@ -2,22 +2,32 @@ import React from "react";
 import "./App.css";
 import _ from "lodash";
 import { ForceGraph3D } from "react-force-graph";
+import graph from "./data/graph.json";
 
 function App() {
   const generateRandomData = () => {
-    const nodes = _.range(0, 10000).map((i) => ({
+    const nodes = _.range(0, 100).map((i) => ({
       id: `id${i}`,
       name: `name${i}`,
       val: Math.random() * 20,
     }));
-    const links = [];
+    const links = _.range(0, 20).map((i) => ({
+      source: `id${_.random(0, 99)}`,
+      target: `id${_.random(0, 99)}`,
+    }));
     return { nodes, links };
   };
 
-  const myData = generateRandomData();
+  const graphData = graph[2020];
+
   return (
     <div className="App">
-      <ForceGraph3D graphData={myData} />
+      <ForceGraph3D
+        graphData={graphData}
+        nodeColor={(d) => {
+          return d.__indexColor;
+        }}
+      />
     </div>
   );
 }
