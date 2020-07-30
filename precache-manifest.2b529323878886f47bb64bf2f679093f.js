370e21ab138ef10f6ece88fcd30a0185ad31010d
Michelle <michmac202@gmail.com>
2020-07-29 19:42:31 -0500

    Updates

---
+++ precache-manifest.2b529323878886f47bb64bf2f679093f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facb8e8324190c954e6eb21a2574b527",
+    "url": "/youtube-map/index.html"
+  },
+  {
+    "revision": "5d0d9f8faeb0724fd85a",
+    "url": "/youtube-map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ed27a0197d4a0551cca",
+    "url": "/youtube-map/static/js/2.80fac561.chunk.js"
+  },
+  {
+    "revision": "9cf5ffcef2b789f813d0536ec8ba9b58",
+    "url": "/youtube-map/static/js/2.80fac561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0d9f8faeb0724fd85a",
+    "url": "/youtube-map/static/js/main.ce6a9e0c.chunk.js"
+  },
+  {
+    "revision": "e403e0508ba1face011d",
+    "url": "/youtube-map/static/js/runtime-main.b82971b5.js"
+  }
+]);
